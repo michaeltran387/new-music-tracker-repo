d64773cc223c97969483056086c01facae7bea26
Michael Tran <michael.tran387@gmail.com>
2024-06-29 18:39:42 -0500

    save 3

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "login"
+    return render_template("login.html", text="Testing")
 
 @auth.route("/logout")
 def logout():
     return "logout"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=[""])
 def sign_up():
-    return "sign up"
+    return render_template("sign_up.html")
